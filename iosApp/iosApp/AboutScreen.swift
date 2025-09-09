6f40ea1d3a5b4f8e9b845d00c71c4a94cf7e1de3
Qkprahlad101 <prahladkumar@Mac-123.lan>
2025-09-09 13:03:28 +0530

    iOS About Screen done, refactored.

--- iosApp/iosApp/AboutScreen.swift
+++ iosApp/iosApp/AboutScreen.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct AboutScreen: View {
     var body: some View {
         NavigationStack{
-            AboutListView().navigationTitle("About Screen")
+            AboutListView().navigationTitle("About Device")
         }
     }
 }
